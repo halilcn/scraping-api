e5df3f3270bf7f1b4ee30187ecaf8d6cb507364a
halilcn <iletisimhalilcan@gmail.com>
2022-12-03 13:04:48 +0300

    add auth middleware

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
+import auth from '../middlewares/auth'
 import adverts from './v1/adverts'
 import users from './v1/users'
 
 module.exports = app => {
-  app.use('/v1/adverts', adverts)
+  app.use('/v1/adverts', auth, adverts)
   app.use('/v1/users', users)
 }
