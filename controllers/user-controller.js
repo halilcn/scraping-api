d5ae8a2f34e6edbc03f7df3c17bc7dedcb1a3778
halilcn <iletisimhalilcan@gmail.com>
2022-12-03 12:54:32 +0300

    update login method

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,8 +1,25 @@
 import asyncHandler from 'express-async-handler'
 import bcrypt from 'bcryptjs'
-import User from '../models/user';
+import jwt from 'jsonwebtoken'
+import User from '../models/user'
+import UserToken from '../models/user-token'
 
-const login = asyncHandler(async (req, res, next) => { })
+const login = asyncHandler(async (req, res, next) => {
+    //const { password = 'test1', email = 'test2' } = req.body //TODO: !
+    const password = 'test1'
+    const email = 'test12@gmail.com'
+
+    const user = await User.findOne({ email })
+
+    const isCorrectPassword = await bcrypt.compareSync(password, user?.password || '')
+    if (!isCorrectPassword) res.status(400).json({ message: 'user does not exist' });
+
+    const token = jwt.sign({ test: 'asd' }, process.env.JWT_TOKEN)
+    await UserToken.deleteOne({ userId: user.id })
+    await UserToken.create({ userId: user.id, token })
+
+    res.status(201).json({ data: { token }, message: 'created' })
+})
 
 const register = asyncHandler(async (req, res, next) => {
     //const { password = 'test1', email = 'test2' } = req.body //TODO: !
