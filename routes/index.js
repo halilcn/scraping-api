fab9a09d7b0d1db3f23fcab63a78012c3acd0c20
halilcn <iletisimhalilcan@gmail.com>
2022-12-04 22:39:09 +0300

    add cuurrent api

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 import auth from '../middlewares/auth'
 import adverts from './v1/adverts'
 import users from './v1/users'
+import currentApi from './../controllers/current-api'
 
 module.exports = app => {
-  app.use('/v1/adverts', auth, adverts)
-  app.use('/v1/users', users)
+  app.use('/api/v1/adverts', auth, adverts)
+  app.use('/api/v1/users', users)
+
+  app.use('/api', currentApi.index)
 }
