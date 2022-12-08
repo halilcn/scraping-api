c2cb8d1a87037fb92aa3a5e241a38579fb0bff53
halilcn <iletisimhalilcan@gmail.com>
2022-12-08 22:49:47 +0300

    update the query filter for advert

--- controllers/advert-controller.js
+++ controllers/advert-controller.js
@@ -2,22 +2,22 @@ import asyncHandler from 'express-async-handler'
 import Advert from '../models/advert'
 
 const index = asyncHandler(async (req, res, next) => {
-  //TODO: tek tek aktarma yapılması?
-  //TODO: room + encode problem
-
   const { query } = req
   const filter = {}
 
   if (query.status) filter.status = query.status
-  if (query.room) filter.room = query.room
+  // if (query.room) filter.room = query.room //TODO: room + encode problem
   if (query.city) filter.city = query.city
   if (query.district) filter.district = query.district
   if (query.furnitureStatus) filter.furnitureStatus = query.furnitureStatus
   if (query.company) filter.company = query.company
-
-  console.log(filter);
+  if (query.minPrice && query.maxPrice) filter.price = { $gte: query.minPrice, $lte: query.maxPrice }
+  if (query.minSquareMetres && query.maxSquareMetres) filter.squareMetres = { $gte: query.minSquareMetres, $lte: query.maxSquareMetres }
+  if (query.minBuildingAge && query.maxBuildingAge) filter.buildingAge = { $gte: query.minBuildingAge, $lte: query.maxBuildingAge }
+  if (query.minDate && query.maxDate) filter.createdAt = { $gte: query.minDate, $lte: query.maxDate }
 
   const adverts = await Advert.find(filter)
+
   res.json({ adverts })
 })
 
